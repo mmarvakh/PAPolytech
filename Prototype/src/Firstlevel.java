0277a7cee7a5219f8773e09d02ed74b0884c0831
MrJaymun <57616388+MrJaymun@users.noreply.github.com>
2020-03-15 16:00:55 +0300

    Text
    Нормально обозначили переменные

--- Prototype/src/Firstlevel.java
+++ Prototype/src/Firstlevel.java
@@ -1,68 +1,68 @@
 public class Firstlevel {
     //Начальные
-    static final String lvl11 = "Я пожарный";
-    static final String lvl12 = "Я врач";
-    static final String lvl13 = "Я учитель";
-    static final String lvl14 = "Я черепаха";
-    static final String lvl15 = "Я не знаю";
-    //Выбран ответ 1
-    static final String lvl211 = "First question lvl 2";
-    static final String lvl212 = "Second question lvl 2";
-    static final String lvl213= "Third question lvl 2";
-    static final String lvl214 = "Fourth question lvl 2";
-    static final String lvl215 = "Fifth question lvl 2";
-    //Выбран ответ 2
-    static final String lvl221 = "First question lvl 2";
-    static final String lvl222 = "Second question lvl 2";
-    static final String lvl223= "Third question lvl 2";
-    static final String lvl224 = "Fourth question lvl 2";
-    static final String lvl225 = "Fifth question lvl 2";
-    //Выбран ответ 3
-    static final String lvl231 = "First question lvl 2";
-    static final String lvl232 = "Second question lvl 2";
-    static final String lvl233= "Third question lvl 2";
-    static final String lvl234 = "Fourth question lvl 2";
-    static final String lvl235 = "Fifth question lvl 2";
-    //Выбран ответ 4
-    static final String lvl241 = "First question lvl 2";
-    static final String lvl242 = "Second question lvl 2";
-    static final String lvl243= "Third question lvl 2";
-    static final String lvl244 = "Fourth question lvl 2";
-    static final String lvl245 = "Fifth question lvl 2";
-    //Выбран ответ 5
-    static final String lvl251 = "First question lvl 2";
-    static final String lvl252 = "Second question lvl 2";
-    static final String lvl253= "Third question lvl 2";
-    static final String lvl254 = "Fourth question lvl 2";
-    static final String lvl255 = "Fifth question lvl 2";
-    //Ответы для 1 типа
-    static final String ans11 = "Answer for 11";
-    static final String ans12 = "Answer for 12";
-    static final String ans13 = "Answer for 13";
-    static final String ans14 = "Answer for 14";
-    static final String ans15 = "Answer for 15";
-    //Ответы для 2 типа
-    static final String ans21 = "Answer for 21";
-    static final String ans22 = "Answer for 22";
-    static final String ans23 = "Answer for 23";
-    static final String ans24 = "Answer for 24";
-    static final String ans25 = "Answer for 25";
-    //Ответы для 3 типа
-    static final String ans31 = "Answer for 31";
-    static final String ans32 = "Answer for 32";
-    static final String ans33 = "Answer for 33";
-    static final String ans34 = "Answer for 34";
-    static final String ans35 = "Answer for 35";
-    //Ответы для 4 типа
-    static final String ans41 = "Answer for 41";
-    static final String ans42 = "Answer for 42";
-    static final String ans43 = "Answer for 43";
-    static final String ans44 = "Answer for 44";
-    static final String ans45 = "Answer for 45";
-    //Ответы для 5 типа
-    static final String ans51 = "Answer for 51";
-    static final String ans52 = "Answer for 52";
-    static final String ans53 = "Answer for 53";
-    static final String ans54 = "Answer for 54";
-    static final String ans55 = "Answer for 55";
+    static final String lvl11 = "Я лесник";
+    static final String lvl12 = "Я заказчик";
+    static final String lvl13 = "Я дерево";
+    static final String lvl14 = "Я житель соседних районов";
+    static final String lvl15 = "Я проверяющий";
+    //Выбран ответ лесник
+    static final String lvl211 = "Что мне нужно?";
+    static final String lvl212 = "Что мне рубить?";
+    static final String lvl213= "Сколько мне рубить";
+    static final String lvl214 = "Чем мне рубить";
+    static final String lvl215 = "Для кого мне рубить?";
+    //Ответы для лесника
+    static final String ans11 = "Вам нужна лицензия на рубку";
+    static final String ans12 = "Рубите деревья";
+    static final String ans13 = "Рубите 5 деревьев в день";
+    static final String ans14 = "Рубите топором";
+    static final String ans15 = "Рубите для вашего босса";
+    //Выбран ответ заказчик
+    static final String lvl221 = "У кого мне заказывать?";
+    static final String lvl222 = "Сколько денег платить?";
+    static final String lvl223= "Как проверить лицензию?";
+    static final String lvl224 = "Какие есть гарантии?";
+    static final String lvl225 = "Куда звонить в случае обмана?";
+    //Ответы для заказчика
+    static final String ans21 = "У проверенных людей";
+    static final String ans22 = "1000$";
+    static final String ans23 = "Посмотрите в базе данных";
+    static final String ans24 = "Никаких гарантий";
+    static final String ans25 = "Звоните в полицию";
+    //Выбран ответ дерево
+    static final String lvl231 = "За что вы меня так?";
+    static final String lvl232 = "Как не быть срубленным?";
+    static final String lvl233= "Во что меня превратят?";
+    static final String lvl234 = "Вы возместите ущерб?";
+    static final String lvl235 = "Я было хорошим деревом?";
+    //Ответы для дерева
+    static final String ans31 = "За Россию!";
+    static final String ans32 = "Никак";
+    static final String ans33 = "В шкаф";
+    static final String ans34 = "Мы посадим много деревьев";
+    static final String ans35 = "Нет. Ты был лучшим";
+    //Выбран ответ житель
+    static final String lvl241 = "Законно ли это?";
+    static final String lvl242 = "Будут ли рубить по ночам?";
+    static final String lvl243 = "Будет ли громко?";
+    static final String lvl244 = "Будут ли платить деньги?";
+    static final String lvl245 = "Кому жаловаться?";
+    //Ответы для жителей
+    static final String ans41 = "Конечно да!";
+    static final String ans42 = "По ночам не работают";
+    static final String ans43 = "Будет не очень громко";
+    static final String ans44 = "Деньги платить не будут";
+    static final String ans45 = "Никому не жалуйтесь пожалуйста <3";
+    //Выбран ответ проверяющий
+    static final String lvl251 = "Как вы это сделали?";
+    static final String lvl252 = "Что вы использовали?";
+    static final String lvl253= "Почему вы добавили сюда эти ответы?";
+    static final String lvl254 = "Как ваше настроение?";
+    static final String lvl255 = "Вы не забудете поменять код?";
+    //Ответы для проверяющего
+    static final String ans51 = "с Божьей помощью";
+    static final String ans52 = "JOptionPane";
+    static final String ans53 = "Потому что фантазия кончилась";
+    static final String ans54 = "Отлично";
+    static final String ans55 = "Надеюсь, что нет";
 }
---
+++ Prototype/src/Window.java
@@ -0,0 +1,179 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+
+class Window {
+    public static void main(String[] args) {
+        begin();
+    }
+
+    public static void begin() {
+        String[] options = {Firstlevel.lvl11, Firstlevel.lvl12, Firstlevel.lvl13, Firstlevel.lvl14, Firstlevel.lvl15,};
+        String n = (String) JOptionPane.showInputDialog(null, "Кто вы?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case (Firstlevel.lvl11):
+                //case111(Firstlevel.lvl111, Firstlevel.lvl112, Firstlevel.lvl113, Firstlevel.lvl114, Firstlevel.115);
+                case11();
+                break;
+            case (Firstlevel.lvl12):
+                case12();
+                break;
+            case (Firstlevel.lvl13):
+                case13();
+                break;
+            case (Firstlevel.lvl14):
+                case14();
+                break;
+            case (Firstlevel.lvl15):
+                case15();
+                break;
+        }
+
+    }
+
+   /* public static void case111( String a, String b, String c, String d,String e ){
+            String[] options = {a, b,c,d,e};
+            String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                    "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+            switch (n) {
+                case (a):
+                    System.out.println(Firstlevel.ans11);
+                    break;
+                case "Second question lvl 2":
+                    System.out.println(Firstlevel.ans12);
+                    break;
+                case "Third question lvl 2":
+                    System.out.println(Firstlevel.ans13);
+                    break;
+                case "Fourth question lvl 2":
+                    System.out.println(Firstlevel.ans14);
+                    break;
+                case "Fifth question lvl 2":
+                    System.out.println(Firstlevel.ans15);
+                    break;
+            }
+        }
+        */
+
+    public static void case11(){
+        String[] options = {Firstlevel.lvl211, Firstlevel.lvl212, Firstlevel.lvl213, Firstlevel.lvl214, Firstlevel.lvl215,};
+        String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case Firstlevel.lvl211:
+                answer(Firstlevel.ans11);
+                break;
+            case Firstlevel.lvl212:
+                answer(Firstlevel.ans12);
+                break;
+            case Firstlevel.lvl213:
+                answer(Firstlevel.ans13);
+                break;
+            case Firstlevel.lvl214:
+                answer(Firstlevel.ans14);
+                break;
+            case Firstlevel.lvl215:
+                answer(Firstlevel.ans15);
+                break;
+        }
+    }
+    public static void case12(){
+        String[] options = {Firstlevel.lvl221, Firstlevel.lvl222, Firstlevel.lvl223, Firstlevel.lvl224, Firstlevel.lvl225,};
+        String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case Firstlevel.lvl221:
+                answer(Firstlevel.ans21);
+                break;
+            case Firstlevel.lvl222:
+                answer(Firstlevel.ans22);
+                break;
+            case Firstlevel.lvl223:
+                answer(Firstlevel.ans23);
+                break;
+            case Firstlevel.lvl224:
+                answer(Firstlevel.ans24);
+                break;
+            case Firstlevel.lvl225:
+                answer(Firstlevel.ans25);
+                break;
+        }
+    }
+    public static void case13(){
+        String[] options = {Firstlevel.lvl231, Firstlevel.lvl232, Firstlevel.lvl233, Firstlevel.lvl234, Firstlevel.lvl235,};
+        String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case Firstlevel.lvl231:
+                answer(Firstlevel.ans31);
+                break;
+            case Firstlevel.lvl232:
+                answer(Firstlevel.ans32);
+                break;
+            case Firstlevel.lvl233:
+                answer(Firstlevel.ans33);
+                break;
+            case Firstlevel.lvl234:
+                answer(Firstlevel.ans34);
+                break;
+            case Firstlevel.lvl235:
+                answer(Firstlevel.ans35);
+                break;
+        }
+    }
+    public static void case14(){
+        String[] options = {Firstlevel.lvl241, Firstlevel.lvl242, Firstlevel.lvl243, Firstlevel.lvl244, Firstlevel.lvl245,};
+        String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case Firstlevel.lvl241:
+                answer(Firstlevel.ans41);
+                break;
+            case Firstlevel.lvl242:
+                answer(Firstlevel.ans42);
+                break;
+            case Firstlevel.lvl243:
+                answer(Firstlevel.ans43);
+                break;
+            case Firstlevel.lvl244:
+                answer(Firstlevel.ans44);
+                break;
+            case Firstlevel.lvl245:
+                answer(Firstlevel.ans45);
+                break;
+        }
+    }
+    public static void case15(){
+        String[] options = {Firstlevel.lvl251, Firstlevel.lvl252, Firstlevel.lvl253, Firstlevel.lvl254, Firstlevel.lvl255,};
+        String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
+                "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+        switch (n) {
+            case Firstlevel.lvl251:
+                answer(Firstlevel.ans51);
+                //System.out.println(Firstlevel.ans51);
+                break;
+            case Firstlevel.lvl252:
+                answer(Firstlevel.ans52);
+                break;
+            case Firstlevel.lvl253:
+                answer(Firstlevel.ans53);
+                break;
+            case Firstlevel.lvl254:
+                answer(Firstlevel.ans54);
+                break;
+            case Firstlevel.lvl255:
+                answer(Firstlevel.ans55);
+                break;
+        }
+    }
+
+    public static void answer(String ans) {
+
+        JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+
+        JOptionPane.showMessageDialog(frame, ans, "Ответ", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}

012bb7585263c4d891bb4a08065f3c5636548db2
kirilla111 <57618503+kirilla111@users.noreply.github.com>
2020-03-17 22:13:31 +0300

    Бесконечная работа программы
    Клавиша "ок" теперь переносит на начальный экран

--- Prototype/src/Window.java
+++ Prototype/src/Window.java
@@ -1,64 +1,40 @@
 import javax.swing.*;
-import java.awt.*;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 
 class Window {
     public static void main(String[] args) {
         begin();
     }
+
     public static void begin() {
         String[] options = {Firstlevel.lvl11, Firstlevel.lvl12, Firstlevel.lvl13, Firstlevel.lvl14, Firstlevel.lvl15,};
         String n = (String) JOptionPane.showInputDialog(null, "Кто вы?",
                 "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
-        switch (n) {
-            case (Firstlevel.lvl11):
-                //case111(Firstlevel.lvl111, Firstlevel.lvl112, Firstlevel.lvl113, Firstlevel.lvl114, Firstlevel.115);
-                case11();
-                break;
-            case (Firstlevel.lvl12):
-                case12();
-                break;
-            case (Firstlevel.lvl13):
-                case13();
-                break;
-            case (Firstlevel.lvl14):
-                case14();
-                break;
-            case (Firstlevel.lvl15):
-                case15();
-                break;
+        try {
+            switch (n) {
+                case (Firstlevel.lvl11):
+                    case11();
+                    break;
+                case (Firstlevel.lvl12):
+                    case12();
+                    break;
+                case (Firstlevel.lvl13):
+                    case13();
+                    break;
+                case (Firstlevel.lvl14):
+                    case14();
+                    break;
+                case (Firstlevel.lvl15):
+                    case15();
+                    break;
+            }
+        } catch (Exception e) {
+            System.exit(0);
         }
 
     }
-
-   /* public static void case111( String a, String b, String c, String d,String e ){
-            String[] options = {a, b,c,d,e};
-            String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
-                    "First level", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
-            switch (n) {
-                case (a):
-                    System.out.println(Firstlevel.ans11);
-                    break;
-                case "Second question lvl 2":
-                    System.out.println(Firstlevel.ans12);
-                    break;
-                case "Third question lvl 2":
-                    System.out.println(Firstlevel.ans13);
-                    break;
-                case "Fourth question lvl 2":
-                    System.out.println(Firstlevel.ans14);
-                    break;
-                case "Fifth question lvl 2":
-                    System.out.println(Firstlevel.ans15);
-                    break;
-            }
-        }
-        */
-
-    public static void case11(){
+    public static void case11 () {
         ImageIcon icon = new ImageIcon("TimberImage.png");
-        String[] options = {Firstlevel.lvl211, Firstlevel.lvl212, Firstlevel.lvl213, Firstlevel.lvl214, Firstlevel.lvl215,Firstlevel.backup};
+        String[] options = {Firstlevel.lvl211, Firstlevel.lvl212, Firstlevel.lvl213, Firstlevel.lvl214, Firstlevel.lvl215, Firstlevel.backup};
         String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
                 "First level", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
         switch (n) {
@@ -81,9 +57,9 @@ class Window {
                 backup();
         }
     }
-    public static void case12(){
+    public static void case12 () {
         ImageIcon icon = new ImageIcon("BusinessImage.png");
-        String[] options = {Firstlevel.lvl221, Firstlevel.lvl222, Firstlevel.lvl223, Firstlevel.lvl224, Firstlevel.lvl225,Firstlevel.backup};
+        String[] options = {Firstlevel.lvl221, Firstlevel.lvl222, Firstlevel.lvl223, Firstlevel.lvl224, Firstlevel.lvl225, Firstlevel.backup};
         String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
                 "First level", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
         switch (n) {
@@ -106,9 +82,9 @@ class Window {
                 backup();
         }
     }
-    public static void case13(){
+    public static void case13 () {
         ImageIcon icon = new ImageIcon("TreeImage.png");
-        String[] options = {Firstlevel.lvl231, Firstlevel.lvl232, Firstlevel.lvl233, Firstlevel.lvl234, Firstlevel.lvl235,Firstlevel.backup};
+        String[] options = {Firstlevel.lvl231, Firstlevel.lvl232, Firstlevel.lvl233, Firstlevel.lvl234, Firstlevel.lvl235, Firstlevel.backup};
         String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
                 "First level", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
         switch (n) {
@@ -131,9 +107,9 @@ class Window {
                 backup();
         }
     }
-    public static void case14(){
+    public static void case14 () {
         ImageIcon icon = new ImageIcon("VillageImage.png");
-        String[] options = {Firstlevel.lvl241, Firstlevel.lvl242, Firstlevel.lvl243, Firstlevel.lvl244, Firstlevel.lvl245,Firstlevel.backup};
+        String[] options = {Firstlevel.lvl241, Firstlevel.lvl242, Firstlevel.lvl243, Firstlevel.lvl244, Firstlevel.lvl245, Firstlevel.backup};
         String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
                 "First level", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
         switch (n) {
@@ -156,9 +132,9 @@ class Window {
                 backup();
         }
     }
-    public static void case15(){
+    public static void case15 () {
         ImageIcon icon = new ImageIcon("WatcherImage.png");
-        String[] options = {Firstlevel.lvl251, Firstlevel.lvl252, Firstlevel.lvl253, Firstlevel.lvl254, Firstlevel.lvl255,Firstlevel.backup};
+        String[] options = {Firstlevel.lvl251, Firstlevel.lvl252, Firstlevel.lvl253, Firstlevel.lvl254, Firstlevel.lvl255, Firstlevel.backup};
         String n = (String) JOptionPane.showInputDialog(null, "What do you want?",
                 "First level", JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
         switch (n) {
@@ -181,13 +157,13 @@ class Window {
                 backup();
         }
     }
-
-    public static void answer(String ans) {
+    public static void answer (String ans){
         ImageIcon icon = new ImageIcon("FSCImage.png");
         JFrame frame = new JFrame("JOptionPane showMessageDialog example");
-        JOptionPane.showMessageDialog(frame, ans, "Ответ", JOptionPane.INFORMATION_MESSAGE, icon);
+        JOptionPane.showMessageDialog(frame, ans, "Ответ", JOptionPane.QUESTION_MESSAGE, icon);
+        begin();
     }
-    public static void backup(){
+    public static void backup () {
         begin();
     }
 }

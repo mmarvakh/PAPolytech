800009480643e915527699f95296a65ed440084e
Marvakh Mark <mmarvakh@gmail.com>
2020-05-06 22:51:32 +0300

    Реализовано появление ответов при нажатии на соответствующие пункты в категориях

--- js/index.js
+++ js/index.js
@@ -7,10 +7,19 @@ const questionList = document.querySelectorAll(".li-question"),
       botMessage = document.createElement("p"),
       answersInCategories = document.createElement("ul");
 
+let questionListWithAnswers = document.querySelectorAll(".question-list");
+
 userMessage.classList.add("message");
 userAnswer.classList.add("user-answer");
-answersInCategories.classList.add("bot-message-green")
 
+botMessage.classList.add("message");
+botAnswer.classList.add("bot-message");
+
+answersInCategories.classList.add("bot-message-green");
+
+let countForLi = 0;
+
+// Категории
 const categoryFirst = {
     questionFirst: "Первый вопрос категории 1",
     questionSecond: "Второй вопрос категории 1",
@@ -39,9 +48,37 @@ const categoryFourth = {
     questionFourth: "Четвёртый вопрос категории 4",
 }
 
-const arrayOfCategories = [categoryFirst, categorySecond, categoryThird, categoryFourth]
+// Ответы
+const answersOnFirstCategory = {
+    0: "Ответ на первый вопрос категории 1",
+    1: "Ответ на второй вопрос категории 1",
+    2: "Ответ на третий вопрос категории 1",
+    3: "Ответ на четвертый вопрос категории 1"
+}
 
-Object.keys(categoryFirst).length
+const answersOnSecondCategory = {
+    0: "Ответ на первый вопрос категории 2",
+    1: "Ответ на второй вопрос категории 2",
+    2: "Ответ на третий вопрос категории 2",
+    3: "Ответ на четвертый вопрос категории 2"
+}
+
+const answersOnThirdCategory = {
+    0: "Ответ на первый вопрос категории 3",
+    1: "Ответ на второй вопрос категории 3",
+    2: "Ответ на третий вопрос категории 3",
+    3: "Ответ на четвертый вопрос категории 3"
+}
+
+const answersOnFourthCategory = {
+    0: "Ответ на первый вопрос категории 4",
+    1: "Ответ на второй вопрос категории 4",
+    2: "Ответ на третий вопрос категории 4",
+    3: "Ответ на четвертый вопрос категории 4"
+}
+
+const arrayOfCategories = [categoryFirst, categorySecond, categoryThird, categoryFourth],
+      arrayOfAnswers = [answersOnFirstCategory, answersOnSecondCategory, answersOnThirdCategory, answersOnFourthCategory];
 
 questionList.forEach((elem) => {
 
@@ -62,25 +99,83 @@ questionList.forEach((elem) => {
 
                 let cloneOfAnswers = answersInCategories.cloneNode(true);
 
+                cloneOfAnswers.classList.add("question-list");
+
                 for (key in element) {
+
                     let li = document.createElement("li");
 
                     li.classList.add("li-question");
 
+                    li.id = countForLi;
+
+                    countForLi++;
+
                     li.textContent = element[key];
 
                     cloneOfAnswers.appendChild(li);
+
                 }
 
-                botContainer.appendChild(cloneOfAnswers)
+                countForLi = 0;
+
+                botContainer.appendChild(cloneOfAnswers);
 
             }
 
-        })
+        });
 
+        questionListWithAnswers = document.querySelectorAll(".question-list");
+
+        questionListWithAnswers.forEach((elemLi) => {
+
+            let li = elemLi.querySelectorAll("li");
         
+            li.forEach((liElem) => {
+        
+                liElem.addEventListener("click", () => {
+        
+                    let cloneOfUserAnswer = userAnswer.cloneNode(true),
+                        cloneOfUserMessage = userMessage.cloneNode(true);
+             
+                    cloneOfUserAnswer.appendChild(cloneOfUserMessage);     
+        
+                    botContainer.appendChild(cloneOfUserAnswer);
 
-    })
+                    cloneOfUserMessage.textContent = liElem.textContent;
+
+                    arrayOfAnswers.forEach((point) => {
+
+                        if (arrayOfAnswers.indexOf(point) == liElem.id) {
+
+                            let cloneOfBotAnswer = botAnswer.cloneNode(true),
+                                cloneOfBotMessage = botMessage.cloneNode(true);
+                            
+                            for (key in point) {
+
+                                if (key == liElem.id) {
+
+                                    cloneOfBotMessage.textContent = point[key];
+
+                                    cloneOfBotAnswer.appendChild(cloneOfBotMessage);
+
+                                }
+
+                            }
+
+                            botContainer.appendChild(cloneOfBotAnswer);
+
+                        }
+
+                    });
+        
+                });
+        
+            });
+        
+        });
+
+    });
 
 });
 
